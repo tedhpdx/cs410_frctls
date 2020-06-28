7331c44f563702f6a52ec57c576809e797714b9a
root <root@lasVentanas10.localdomain>
2020-06-28 16:04:51 -0700

    commit

--- tangent_circles_a.c
+++ tangent_circles_a.c
@@ -16,27 +16,41 @@ int main()
   G_rgb(1,0,0);
 
   G_wait_click(p);
-  G_fill_circle(p[0],p[1],20);
 
   G_wait_click(q);
-  G_fill_circle(q[0],q[1],20);
 
   G_rgb(0,1,0);
   G_line(p[0],p[1] , q[0],q[1]);
-
-  //midpoint cicle
-  double midx = (p[0]+q[0])/2;
-  double midy = (p[1]+q[1])/2;
-
-  G_rgb(1,0,0);
-  G_fill_circle(midx,midy,20);
-
 
   //distance of line
   double deltaX = p[0]-q[0];
   double deltaY = p[1]-q[1];
 
   double dist = sqrt((deltaX*deltaX)+(deltaY*deltaY));
+  double r = dist/16;
+
+  //original circles
+  G_rgb(1,0,0);
+  G_fill_circle(p[0],p[1],r);
+  G_fill_circle(q[0],q[1],r);
+  //midpoint cicle
+  double midx = (p[0]+q[0])/2;
+  double midy = (p[1]+q[1])/2;
+
+  G_fill_circle(midx,midy,r);
+
+  //midpoint again
+  double midx1 = (p[0]+midx)/2;
+  double midy1 = (p[1]+midy)/2;
+
+  double midx2 = (q[0]+midx)/2;
+  double midy2 = (q[1]+midy)/2;
+
+  G_fill_circle(midx1,midy1,r);
+  G_fill_circle(midx2,midy2,r);
+
+
+
 
 
   G_wait_key();
